f1791318f7234da823768718f8f813d7429fdd10
Anelia Vasileva <anel4610@abv.bg>
2022-11-02 08:30:55 +0200

    basicSyntaxConditionalStatementsAndLoops-exercise

---
+++ basicSyntaxConditionalStatementsAndLoops-exercise/triangleOfNumbers.js
@@ -0,0 +1,12 @@
+function triangleOfNumbers(n) {
+  for (let row = 1; row <= n; row++) {
+    let printLine = "";
+    for (let col = 1; col <= row; col++) {
+      printLine += `${row} `;
+    }
+    console.log(printLine);
+  }
+}
+triangleOfNumbers(3);
+triangleOfNumbers(5);
+triangleOfNumbers(6);
